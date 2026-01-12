6c2b5728f153b9c091a0509ee07ecbad60ca2175
Darién Julián Ramírez <ianshalaga@gmail.com>
2026-01-12 18:47:59 +0100

    Update base.py

--- services/ranking/storage/base.py
+++ services/ranking/storage/base.py
@@ -1,3 +1,11 @@
+'''
+Convención de Naming ORM
+
+- Las clases ORM representan una entidad individual → singular
+- Las tablas representan colecciones → plural
+- Los nombres de relaciones siguen el plural cuando corresponda
+'''
+
 from sqlalchemy.orm import DeclarativeBase
 
 
